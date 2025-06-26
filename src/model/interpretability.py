d614cf1491efbeb3eaaba53360820f92bd5ab637
Getachew Ganfur <getachewganfur@gmail.com>
2025-06-26 23:13:04 +0300

    fix(src): modify code

--- src/model/interpretability.py
+++ src/model/interpretability.py
@@ -5,7 +5,7 @@ from transformers import pipeline
 # Load model and tokenizer
 ner_pipeline = pipeline("ner", model="ner_finetuned_model", tokenizer="xlm-roberta-base", aggregation_strategy="simple")
 
-# Example: SHAP for a single prediction
+# Example: SHAP for a single  prediction
 explainer = shap.Explainer(ner_pipeline)
 shap_values = explainer(["ሶፋ ዋጋ 1000 ብር ቦሌ ላይ አለ"])
 
